2409f4200efa01eedc2dbdadc15a34eadcc6e30a
Nicholas Savory <nicholas-savory@outlook.com>
2016-06-17 15:57:04 -0400

    Add index
    Add index.php and CSS

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+  <head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-aNUYGqSUL9wG/vP7+cWZ5QOM4gsQou3sBfWRr/8S3R1Lv0rysEmnwsRKMbhiQX/O" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <title>Canadian Census w/ Hobbies</title>
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="header clearfix">
+      <div class="page-header">
+        <nav>
+         <ul class="nav nav-pills pull-right">
+           <li role="presentation" class="active"><a href="index.php">Home</a></li>
+           <li role="presentation"><a href="new_person.php">Add Person</a></li>
+           <li role="presentation"><a href="new_hobby.php">Add Hobby</a></li>
+           <li role="presentation"><a href="database.php">Database</a></li>
+         </ul>
+        </nav>
+        <h1>TEST</h1>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </body>
+
+</html>
